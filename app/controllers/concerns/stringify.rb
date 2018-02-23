8d6d01bd7d407de4f385444e60ded2f1064791ff
Min Jung Kim <mmkim801@berkeley.edu>
2018-02-22 17:48:26 -0800

    finished

--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -1,12 +1,17 @@
 class Stringify
-  attr_accessor :name, :adjective
+	attr_accessor :name, :adjective
 
-  def initialize(name, adjective)
-    @name = name
-    @adjective = adjective
-  end
+	def initialize(name, adjective)
+		@name = name
+		@adjective = adjective
+	end
 
-  def what_am_i
-  	# YOUR IMPLEMENTATION HERE
-  end
+	def what_am_i
+		# YOUR IMPLEMENTATION HERE
+		if name.blank? || adjective.blank?
+			"You are nothing!"
+		else
+			name + " is so " + adjective
+		end
+	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,12 @@ class PagesController < ApplicationController
   def person
     @person = Person.new params[:name], params[:age]
   end
+
+  def me
+    @name = "Name: Min Jung Kim"
+    @from = "From: SoCal"
+    @year = "Year: Senior"
+    @fun = "Fun Fact: Going to Mexico this Spring"
+    @excite = "Excited to become a ruby expert!"
+  end
 end
